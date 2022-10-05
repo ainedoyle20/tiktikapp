918b67ce24703e6abd8a1a513376e636a4d935a5
Aine Doyle <ainedoyleAD@gmail.com>
2022-10-05 14:30:32 +0100

    to update vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Video } from '../types';
 import VideoCard from '../components/VideoCard';
 import NoResults from '../components/NoResults';
 
-import {BASE_URL} from '../utils';
+import { BASE_URL } from '../utils';
 
 interface IProps {
   videos: Video[];
@@ -13,7 +13,8 @@ interface IProps {
 const Home = ({ videos }: IProps) => {
   return (
     <div className='flex flex-col gap-10 videos h-full'>
-      {videos?.length ? (
+      {videos?.length 
+      ? (
         videos.map((video:Video) => (
           <VideoCard key={video._id} post={video} />
         ))
